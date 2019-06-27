6891385007fe7086662a6b2fe3c6f9043f3d2fd2
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-06-27 18:47:26 +0400

    Add files via upload

---
+++ sketch_jun27a.ino
@@ -0,0 +1,21 @@
+int a = 2;
+int b=7;
+int c=12;
+void setup() {
+ 
+pinMode (a,OUTPUT);
+pinMode (b,OUTPUT);
+pinMode (c,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(a,HIGH);
+  delay(50);
+    digitalWrite(a,LOW);
+    digitalWrite(b,HIGH);
+  delay(50);
+    digitalWrite(b,LOW);
+    digitalWrite(c,HIGH);
+  delay(50);
+    digitalWrite(c,LOW);
+}
